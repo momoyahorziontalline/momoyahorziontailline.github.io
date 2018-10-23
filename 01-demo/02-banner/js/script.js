b3f6ef22989b04e85402dba3db18bbe5f40ab6d8
Ya-Jie,Huang <momo90030@yahoo.com.tw>
2018-10-24 01:39:24 +0800

    update

---
+++ 01-demo/02-banner/js/script.js
@@ -0,0 +1,23 @@
+var swiper = new Swiper('.swiper-container', {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    breakpoints: {
+        375: {
+            slidesPerView: 1,
+            spaceBetween: 100,
+        }
+    },
+
+});
