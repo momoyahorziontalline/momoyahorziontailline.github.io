c2f9412c68179c15f036691df29ec341c229b975
Ya-Jie,Huang <momo90030@yahoo.com.tw>
2018-11-08 00:55:24 +0800

    update

---
+++ LovelyBear/js/swiper.js
@@ -0,0 +1,21 @@
+var swiper = new Swiper('.swiper-container', {
+    slidesPerView: 2,
+    spaceBetween: 0,
+    slidesPerGroup: 2,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    breakpoints: {
+        576: {
+            slidesPerView: 1,
+            spaceBetween: 10,
+        }
+    },
+});
