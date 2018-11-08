6d92510aeac78a093c68c5dac2f01b36f74c2331
Ya-Jie,Huang <momo90030@yahoo.com.tw>
2018-11-08 18:04:33 +0800

    update

--- LovelyBear/js/swiper.js
+++ LovelyBear/js/swiper.js
@@ -1,3 +1,4 @@
+//swiper
 var swiper = new Swiper('.swiper-container', {
     slidesPerView: 2,
     spaceBetween: 0,
@@ -19,3 +20,23 @@ var swiper = new Swiper('.swiper-container', {
         }
     },
 });
+
+//banner
+$('.carousel').carousel({
+    interval: 2000
+});
+
+// section_navbar 滾動效果
+var intViewportHeight = window.innerHeight;
+var navFixHeight = intViewportHeight - 5;
+
+window.onscroll = function () { myFunction() };
+
+function myFunction() {
+    if (document.body.scrollTop > navFixHeight || document.documentElement.scrollTop > navFixHeight) {
+        /*當卷軸滾動超過50px時，執行以下程式*/
+        document.querySelector("#navbar_1").classList.add('fixed');
+    } else {
+        document.querySelector("#navbar_1").classList.remove('fixed');
+    }
+}
