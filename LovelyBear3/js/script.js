fbd891ae20c316bfc095cebb5c9e1b3bdddd24c3
Ya-Jie,Huang <momo90030@yahoo.com.tw>
2018-11-20 18:08:08 +0800

    up

---
+++ LovelyBear3/js/script.js
@@ -0,0 +1,84 @@
+//******************
+//*     jquery
+//******************
+//navbar
+$('.navbarTrigger').click(function () {
+  $(this).toggleClass('active');
+  // console.log("Clicked menu");
+  $("#mainListDiv").toggleClass("show_list");
+  $("#mainListDiv").fadeIn();
+
+});
+
+//收合
+$('.main_list ul li a').click(function () {
+  /* always close responsive nav after click */
+  $('.navbarTrigger:visible').click();
+});
+
+//banner
+$(function () {
+  var $item = $('.carousel .carousel-item');
+  var $wHeight = $(window).height();
+  $item.height($wHeight);
+  $item.addClass('full-screen');
+
+  $('.carousel img').each(function () {
+    var $src = $(this).attr('src');
+    var $color = $(this).attr('data-color');
+    $(this).parent().css({
+      'background-image': 'url(' + $src + ')',
+      'background-color': $color
+    });
+    $(this).remove();
+  });
+
+  //縮放視窗調整視窗高度
+  $(window).on('resize', function () {
+    $wHeight = $(window).height();
+    $item.height($wHeight);
+  });
+});
+
+// 跑馬燈marquee
+$(function () {
+  var $marqueeUl = $('div#marquee ul'),
+    $marqueeli = $marqueeUl.append($marqueeUl.html()).children(),
+    _height = $('div#marquee').height() * -1,
+    scrollSpeed = 600,
+    timer,
+    speed = 3000 + scrollSpeed;
+
+  $marqueeli.hover(function () {
+    clearTimeout(timer);
+  }, function () {
+    timer = setTimeout(showad, speed);
+  });
+
+  function showad() {
+    var _now = $marqueeUl.position().top / _height;
+    _now = (_now + 1) % $marqueeli.length;
+
+    // $marqueeUl 移動
+    $marqueeUl.animate({
+      top: _now * _height
+    }, scrollSpeed, function () {
+
+      if (_now == $marqueeli.length / 2) {
+        $marqueeUl.css('top', 0);
+      }
+    });
+
+    timer = setTimeout(showad, speed);
+  }
+
+  timer = setTimeout(showad, speed);
+
+  $('a').focus(function () {
+    this.blur();
+  });
+});
+
+
+
+
---
+++ LovelyBear3/js/swiper.js
@@ -0,0 +1,105 @@
+//AOS初始化
+AOS.init();
+
+//swiper
+var swiper = new Swiper('.swiper-container', {
+    slidesPerView: 2,
+    spaceBetween: 0,
+    slidesPerGroup: 2,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    breakpoints: {
+        576: {
+            slidesPerView: 1,
+            spaceBetween: 10,
+            slidesPerGroup: 1,
+        }
+    },
+});
+
+//banner
+$('.carousel').carousel({
+    interval: 2000
+});
+
+// section_navbar 滾動效果
+var intViewportHeight = window.innerHeight;
+var navFixHeight = intViewportHeight - 100;
+
+window.onscroll = function () { myFunction() };
+
+function myFunction() {
+    if (document.body.scrollTop > navFixHeight || document.documentElement.scrollTop > navFixHeight) {
+        /*當卷軸滾動超過50px時，執行以下程式*/
+        console.log("123");
+
+        document.querySelector("#navbar_1").classList.add('fixed');
+    } else {
+        document.querySelector("#navbar_1").classList.remove('fixed');
+    }
+}
+
+//backtop
+$('.backtop').hide();
+
+//button fade in fade out 
+$(document).scroll(function () {
+
+    if ($(this).scrollTop() > 200) {
+        $('.backtop').fadeIn();
+    } else {
+        $('.backtop').fadeOut();
+    }
+});
+
+//animate scroll to top
+$('.backtop').on('click', function () {
+    $('body,html').animate({
+        scrollTop: 0
+    }, 500)
+});
+
+//tab
+var btn1 = document.querySelector('#btn1');
+var btn2 = document.querySelector('#btn2');
+var btn3 = document.querySelector('#btn3');
+var btn4 = document.querySelector('#btn4');
+var tab_content1 = document.querySelector('#tab_content1');
+var tab_content2 = document.querySelector('#tab_content2');
+var tab_content3 = document.querySelector('#tab_content3');
+var tab_content4 = document.querySelector('#tab_content4');
+
+
+btn1.onclick = function () {
+  tab_content1.classList.add("show");
+  tab_content2.classList.remove("show");
+  tab_content3.classList.remove("show");
+  tab_content4.classList.remove("show");
+}
+btn2.onclick = function () {
+  tab_content1.classList.remove("show");
+  tab_content2.classList.add("show");
+  tab_content3.classList.remove("show");
+  tab_content4.classList.remove("show");
+}
+btn3.onclick = function () {
+  tab_content1.classList.remove("show");
+  tab_content2.classList.remove("show");
+  tab_content3.classList.add("show");
+  tab_content4.classList.remove("show");
+}
+btn4.onclick = function () {
+  tab_content1.classList.remove("show");
+  tab_content2.classList.remove("show");
+  tab_content3.classList.remove("show");
+  tab_content4.classList.add("show");
+}
+
