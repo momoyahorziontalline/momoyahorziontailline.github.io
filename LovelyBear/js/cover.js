e0853221b115daef5e31660aaad6ce7fd950f26c
Ya-Jie,Huang <momo90030@yahoo.com.tw>
2018-11-22 18:45:10 +0800

    up

--- LovelyBear/js/cover.js
+++ LovelyBear/js/cover.js
@@ -1,6 +1,6 @@
 //cover
 var cover = new TimelineMax();
-cover.to("#cover", 0, { center: 0 })
-.to("#cover", 5.0, { opacity: 0, scale: 1 }, 0.4)
+cover.to(".cover", 0, { center: 0 })
+.to(".cover", 5.0, { opacity: 0, scale: 1 }, 0.4)
 .to("#section_cover", 5.0, { height: 0 }, 2)
-.to("#cover", 5.0, { height: 0 }, 1.5);
+.to(".cover", 5.0, { height: 0 }, 1.5);
